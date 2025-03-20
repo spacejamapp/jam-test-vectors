0f95377965d4658b43085d9c4e58a845e5c0a76b
clearloop <tianyi.gc@gmail.com>
2025-03-21 00:07:18 +0800

    chore(pvm): remove pvm

--- pvm/validate-json-schema.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Requires yajsv to be installed. You can install it with:
-#   go install github.com/neilpa/yajsv@latest
-
-set -euo pipefail
-~/go/bin/yajsv -s schema.json programs/*.json
